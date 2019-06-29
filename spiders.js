d4cdde0a735efd5f64078eb3cee7dce4b091cb44
yaoyue <yaoyue66@qq.com>
2019-06-29 14:35:22 +0800

    spiderJS

---
+++ spiders.js
@@ -0,0 +1,19 @@
+const originRequest = require("request");
+const cheerio = require("cheerio");
+const iconv = require("iconv-lite");
+
+function request(url, callback) {
+  const options = {
+    url: url,
+    encoding: null
+  };
+  originRequest(url, options, callback);
+}
+for (let i = 100570; i < 100580; i++) {
+  const url = `https://www.dy2018.com/i/${i}.html`;
+  request(url, function(err, res, body) {
+    const html = iconv.decode(body, "gb2312");
+    const $ = cheerio.load(html);
+    console.log($(".title_all h1").text());
+  });
+}
